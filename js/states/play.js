8bad8c67addc1d799c81dec57cdacdb4103cb8d3
Niclas Bodenmann <niclas.bodenmann@uzh.ch>
2019-04-27 12:27:46 +0200

    Implemented a way to travel from isle and to isle

--- js/states/play.js
+++ js/states/play.js
@@ -6,10 +6,56 @@ game.PlayStage = me.Stage.extend({
         me.game.world.reset();
         me.game.world.addChild(new me.ColorLayer("background", "#000000"), 0)
         me.game.world.addChild(this.player);
-        //TODO: AddChilde(Island),
-        this.currentInd = 0
+        
+        //The Setting of the Island
+        this.currentInd = 0;
+        this.islandArray = generateIslandArray();
+
+        //The Init of a single island
+        if (this.islandArray[this.currentInd] == 0) {
+            this.currentIsl = new game.GoodIsland(this.currentInd)
+        } else {
+            this.currentIsl = new game.BadIsland(this.currentInd)
+        }
+
+        this.currentIsl.start(nextIsland);
+        me.game.world.addChild(this.currentIsl);
 
     },
+
+    lastIsland = function() {
+        //TODO: Transition to end
+        console.log('you know what? FUCK you')
+
+    },
+
+    nextIsland = function(){
+
+        this.currentInd++
+
+        me.game.world.removeChild(this.currentIsl)
+        
+        if (this.islandArray[this.currentInd] == 0) {
+            this.currentIsl = new game.GoodIsland(this.currentInd)
+        } else {
+            this.currentIsl = new game.BadIsland(this.currentInd)
+        }
+
+        if (this.currentInd == this.islandArray.length) {
+            this.currentIsl.start(lastIsland);
+        } else {
+            this.currentIsl.start(nextIsland);
+        }
+
+        me.game.world.addChild(this.currentIsl);
+
+    },
+
+    generateIslandArray = function() {
+        // TODO: Randomizer 
+        return [0,1,0,1,0,1];
+    },
+
 
     onDestroyEvent: function() {
 
