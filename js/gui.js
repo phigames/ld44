c664b423022bf3beb144b8f3ca857b7ea805bba0
Andreas Säuberli <andreas.saeuberli@gmail.com>
2019-04-27 12:39:48 +0200

    add button

---
+++ js/gui.js
@@ -0,0 +1,13 @@
+game.GUI = {};
+
+game.GUI.Button = me.Renderable.extend({
+    init: function(x, y, text, onClick) {
+        this._super(me.Container, "init", [x, y, 50, 50]);
+        this.color = '#FF0000';
+    },
+
+    draw: function(renderer) {
+        renderer.setColor(this.color);
+        renderer.fillRect(0, 0, this.width, this.height);
+    }
+});
