f707cc6ffe2366850d870df31d848b067c6d1e65
Andreas Säuberli <andreas.saeuberli@gmail.com>
2019-04-28 20:51:57 +0200

    wave animation

--- js/animation.js
+++ js/animation.js
@@ -76,11 +76,18 @@ game.OscillatingSprite = me.Sprite.extend({
         this.oscillateX = oscillateX;
         this.oscillateY = oscillateY;
         this.oscillatePeriod = oscillatePeriod;
+        let phase = Math.random() * oscillatePeriod * 2;
         new me.Tween(this.pos)
-            .to({ x: this.pos.x + this.oscillateX,
-                  y: this.pos.y + this.oscillateY },
+            .to({ x: this.pos.x + this.oscillateX },
                 this.oscillatePeriod)
-            .easing(me.Tween.Easing.Quadratic.InOut)
+            .easing(me.Tween.Easing.Sinusoidal.InOut)
+            .repeat(Infinity)
+            .yoyo(true)
+            .start();
+        new me.Tween(this.pos)
+            .to({ y: this.pos.y + this.oscillateY },
+                this.oscillatePeriod)
+            .easing(me.Tween.Easing.Sinusoidal.InOut)
             .repeat(Infinity)
             .yoyo(true)
             .start();
